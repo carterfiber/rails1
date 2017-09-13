d2eed84e3054912ef85fee566de06cf19e479010
carterfiber <gerald@carterfiber.com>
2017-09-13 10:23:10 -0400

    added pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  root 'welcome#index'
+  get 'home' => 'welcome#index'
 
-  get 'welcome/about'
+  get 'about' => 'welcome#about'
 
-  get 'welcome/contact'
+  get 'contact' => 'welcome#contact'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
